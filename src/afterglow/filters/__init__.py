e6347d2abd4180419f7e6da1687a82f9bf3f8b0c
Juan Flores <rajdesai58.work@gmail.com>
2025-02-12 14:00:00 +0000

    chore: scaffold afterglow-lab with CLI and filters
    - add typr CLI with halftone, perlin-warp, kaleidoscope commands
    - minimal filters implementation (first cut)
    - basic README and examples folder

---
+++ src/afterglow/filters/__init__.py
@@ -0,0 +1,9 @@
+from .halftone import halftone_dots
+from .perlin_warp import perlin_warp
+from .kaleidoscope import kaleidoscope
+
+__all__ = [
+    "halftone_dots",
+    "perlin_warp",
+    "kaleidoscope",
+]
